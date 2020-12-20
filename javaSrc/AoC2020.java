95e67b794ecd31eadafd3570f5e99bf256d2ff8f
JimHofm52 <JimHofmann52@gmail.com>
2020-12-20 17:29:17 -0500

    Finished 9 & some more cleanup

--- javaSrc/AoC2020.java
+++ javaSrc/AoC2020.java
@@ -6,17 +6,19 @@ Date Created: 12/5/2020
 
 import java.io.*;
 // import days.AllDays;
-import days.Day5;   //<===== Change to match day
-import days.Day6;   //<===== Change to match day
-import days.Day7;   //<===== Change to match day
-import days.Day8;   //<===== Change to match day
+import days.Day5;
+import days.Day6;
+import days.Day7;
+import days.Day8;
+import days.Day9;
 
 public class AoC2020 {
     public static void main(String[] arg) throws IOException{
 
-        Day5.update();  //<===== Change to match day
+        // Day5.update();  //<===== Change to match day
         // Day6.update();  //<===== Change to match day
         // Day7.update();  //<===== Change to match day
         // Day8.update();  //<===== Change to match day
+        Day9.update();  //<===== Change to match day
     }
 }
